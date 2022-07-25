9f405080f0a6bf8b458d2329e1033a7d0400e027
Oxypteros <oxypteros@gmail.com>
2022-07-25 13:26:03 +0200

    left-side minus logo rready

--- lab/alpha/js/app.js
+++ lab/alpha/js/app.js
@@ -4,6 +4,11 @@ const widthFiftyAll = document.querySelectorAll('.w50');
 const darkLogo = document.querySelector('#dark-logo')
 const leftBtn = document.querySelector('#left-btn');
 const rightBtn = document.querySelector('#right-btn');
+const preTitle = document.querySelector('.pre-title');
+const title = document.querySelector('.title');
+const preRandom = document.querySelector('.pre-random');
+const randomBtn = document.querySelector('#random-btn');
+const footnote = document.querySelector('.footnote');
 
 // Mouse on Left Frame
 leftFrame.addEventListener('mousemove', () => {
@@ -11,8 +16,13 @@ leftFrame.addEventListener('mousemove', () => {
   w50.classList.remove('w50');
   });
   leftFrame.classList.add('w100');
-  rightFrame.classList.add('w0')
+  rightFrame.classList.add('w0');
+  preTitle.classList.remove('visually-hidden');
+  title.classList.add('underline');
+  preRandom.classList.remove('visually-hidden');
+  randomBtn.classList.remove('visually-hidden')
   leftBtn.classList.remove('hidden');
+  footnote.classList.remove('hidden');
 });
 
 // Left button pressed
@@ -21,7 +31,12 @@ leftBtn.addEventListener('click', () => {
   leftFrame.classList.add('w50');
   rightFrame.classList.remove('w0');
   rightFrame.classList.add('w50');
+  preTitle.classList.add('visually-hidden');
+  title.classList.remove('underline');
+  preRandom.classList.add('visually-hidden');
+  randomBtn.classList.add('visually-hidden')
   leftBtn.classList.add('hidden')
+  footnote.classList.add('hidden');
 });
 
 // Mouse on Right Frame
