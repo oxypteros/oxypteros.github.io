2d22adcdea12cd1a3615af4678df0bd4505d1446
oxypteros@gmail.com <oxypteros@gmail.com>
2022-07-31 01:44:06 +0200

    progress reading bar

--- lab/alpha/js/tester.js
+++ lab/alpha/js/tester.js
@@ -1,4 +1,20 @@
-const all =document.querySelectorAll('.test, .test1')
+const readingProgress = document.querySelector('#reading-progress-fill');
+const value = document.querySelector('#file').value;
+console.log(value)
+document.addEventListener('scroll', function(e) {
+  let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+  document.querySelector('#file').value += w;
+  value.setAttribute(w);
+  readingProgress.style.setProperty('width', w + '%');
+});
+let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+console.log(w)
+
+
+
+
+
+/*const all =document.querySelectorAll('.test, .test1')
 const btn = document.querySelector('#btn');
 console.log(all);
 console.log(all.length);
@@ -10,3 +26,4 @@ btn.addEventListener('click', function() {
   test1.classList.add('c')
  })
 });
+*/
