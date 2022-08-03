495e5f60659d775b815b93955891be23617b7454
oxypteros@gmail.com <oxypteros@gmail.com>
2022-08-03 15:06:36 +0200

    card recent

--- lab/alpha/js/tester.js
+++ lab/alpha/js/tester.js
@@ -2,17 +2,21 @@
 const start = document.querySelector('.start');
 const end = document.querySelector('.end');
 let value = document.querySelector('#file').value;
-console.log('Start Value = ' + value);
+const totalY = document.body.scrollHeight;
+
+console.log('Start Value = ' + totalY);
 const startPointY = start.offsetTop;
 const endPointY = end.offsetTop;
 const finalHeight = endPointY - startPointY;
-
+const y = totalY - endPointY - startPointY;
+console.log(y)
+console.log(endPointY)
 window.addEventListener('scroll', function() {
   const scrollPositionY = window.pageYOffset;
   
   if (endPointY > scrollPositionY && startPointY < scrollPositionY ) {
 
-    let percentY = scrollPositionY * 100 / finalHeight;
+    let percentY = (scrollPositionY + y) * 100 / finalHeight;
     console.log('Percent = ' + percentY)
 
   document.querySelector('#file').setAttribute('value', percentY);
