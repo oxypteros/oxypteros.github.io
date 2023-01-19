e5fe1cba7e41582ca32eceb61198ffd27a3dfbfb
Oxypteros <oxypteros@gmail.com>
2023-01-19 13:12:02 +0100

    update menu

--- lab/bnb/js/main.js
+++ lab/bnb/js/main.js
@@ -1,45 +1,104 @@
 'use strict';
-// Bottom bar constants
-const hamMenuSmall = document.getElementById('hamMenuSmallJs');
+/* Constants for the three screen sizes nav bars (small, medium, large) */
+const navSmall = document.querySelector('.nav-small');
+const navMedium = document.querySelector('.nav-medium');
+const navLarge = document.querySelector('.nav-large');
+/* Check what size nav bar is shown to the user */
+const checkNavSmall = window.getComputedStyle(navSmall).display;
+const checkNavMedium = window.getComputedStyle(navMedium).display;
+const checkNavLarge = window.getComputedStyle(navLarge).display;
+/* Constant for when the user click outside of the menus */
+const main = document.querySelector('.mainJs');
 
-// Hamburger Menu
-if (hamMenuSmall) {
+/* Hamburger Nav Menu for SMALL screens.*/
+// If small nav bar is shown
+if (checkNavSmall === 'block') {
+  // Open/close buttons
   const hamBtnSmall = document.getElementById('hamBtnSmallJs');
+  const closeBtnSmall = document.getElementById('closeBtnSmallJs');
+  // Button bar and nav menu container
+  const navBtnBarSmall = document.getElementById('navBarSmallJs')
+  const navMenuSmall = document.getElementById('navMenuSmallJs')
+  // Open small hamburger menu function 
+  function openSmallMenu() {
+    // Hide button bar and show nav menu container
+    if(navMenuSmall.classList.contains('hidden')) {
+      navBtnBarSmall.classList.add('hidden');
+      navMenuSmall.classList.remove('hidden');
+      }
+    };
+  // Close small nav menu function 
+  function closeSmallMenu() {
+    // Hide nav menu container and show button bar
+    if(navBtnBarSmall.classList.contains('hidden')) {
+      navMenuSmall.classList.add('hidden');
+      navBtnBarSmall.classList.remove('hidden');
+    }
+  };
+  // Event listeners for open close buttons and click outside of bounds
+  hamBtnSmall.addEventListener('click', openSmallMenu);
+  closeBtnSmall.addEventListener('click', closeSmallMenu);
+  main.addEventListener('click', closeSmallMenu);
+}
 
- 
-  //const hamBtnClose = document.querySelector('.hamBtnCloseJs');
-  //const menuBar = document.querySelector('.menuBarJs');
-  //const navBar = document.querySelector('.navBarJs');
-  //const droid = document.querySelector('.droidJs');
-  
-  // Open Hamburger Menu function
-  function openHamMenu() {
-    if(navBar.classList.contains('hiddenJs')) {
-      menuBar.classList.add('hiddenJs');
-      //droid.classList.add('slideIn');
-      navBar.classList.add('slideIn');
-      navBar.classList.remove('hiddenJs');
-      setTimeout(function() { 
-      //droid.classList.remove('slideIn');
-      navBar.classList.remove('slideIn'); 
-      }, 1000);
+/* Hamburger Nav Menu for MEDIUM screens.*/
+// If medium nav bar is shown
+if (checkNavMedium === 'block') {
+  // Open/close buttons
+  const hamBtnMedium = document.getElementById('hamBtnMediumJs');
+  const closeBtnMedium = document.getElementById('closeBtnMediumJs');
+  // Button bar and nav menu container
+  const navBtnBarMedium = document.getElementById('navBarMediumJs')
+  const navMenuMedium = document.getElementById('navMenuMediumJs')
+  // Open medium hamburger menu function 
+  function openMediumMenu() {
+    // Hide button bar and show nav menu container
+    if(navMenuMedium.classList.contains('hidden')) {
+      navBtnBarMedium.classList.add('hidden');
+      navMenuMedium.classList.remove('hidden');
+      }
+    };
+  // Close small nav menu function 
+  function closeMediumMenu() {
+    // Hide nav menu container and show button bar
+    if(navBtnBarMedium.classList.contains('hidden')) {
+      navMenuMedium.classList.add('hidden');
+      navBtnBarMedium.classList.remove('hidden');
     }
   };
-  // Close Hamburger Menu function
-  function closeHamMenu() {
-    if(menuBar.classList.contains('hiddenJs')) {
-      navBar.classList.add('hiddenJs');
-      menuBar.classList.remove('hiddenJs');
+  // Event listeners for open close buttons and click outside of bounds
+  hamBtnMedium.addEventListener('click', openMediumMenu);
+  closeBtnMedium.addEventListener('click', closeMediumMenu);
+  main.addEventListener('click', closeMediumMenu);
+}
+
+/* Hamburger Nav Menu for LARGE screens.*/
+// If large nav bar is shown
+if (checkNavLarge === 'block') {
+  // Open/close buttons
+  const hamBtnLarge = document.getElementById('hamBtnLargeJs');
+  const closeBtnLarge = document.getElementById('closeBtnLargeJs');
+  // Button bar and nav menu container
+  const navBtnBarLarge = document.getElementById('navBarLargeJs')
+  const navMenuLarge = document.getElementById('navMenuLargeJs')
+  // Open medium hamburger menu function 
+  function openLargeMenu() {
+    // Hide button bar and show nav menu container
+    if(navMenuLarge.classList.contains('hidden')) {
+      navBtnBarLarge.classList.add('hidden');
+      navMenuLarge.classList.remove('hidden');
+      }
+    };
+  // Close small nav menu function 
+  function closeLargeMenu() {
+    // Hide nav menu container and show button bar
+    if(navBtnBarLarge.classList.contains('hidden')) {
+      navMenuLarge.classList.add('hidden');
+      navBtnBarLarge.classList.remove('hidden');
     }
   };
-  function closeOutside() {
-    if(index == null){
-      navBar.classList.add('hiddenJs');
-      menuBar.classList.remove('hiddenJs');
-    }else{
-          if(menuBar.classList.contains('hiddenJs') && index.classList.contains('hiddenJs')) {
-            navBar.classList.add('hiddenJs');
-            menuBar.classList.remove('hiddenJs');
-    } 
-  }
-  };}
+  // Event listeners for open close buttons and click outside of bounds
+  hamBtnLarge.addEventListener('click', openLargeMenu);
+  closeBtnLarge.addEventListener('click', closeLargeMenu);
+  main.addEventListener('click', closeLargeMenu);
+}
