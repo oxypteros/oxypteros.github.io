0486112713051b526598acd34f59881a301ff953
Oxypteros <oxypteros@gmail.com>
2023-01-23 16:24:55 +0100

    carousel

--- lab/bnb/js/main.js
+++ lab/bnb/js/main.js
@@ -74,6 +74,18 @@ if (checkNavMedium === 'block') {
   cover.addEventListener('click', closeSmallMenu);
   main.addEventListener('click', closeMediumMenu);
 }
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+nextButton.addEventListener("click", (event) => {
+  const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
 /*
 // Hamburger Nav Menu for LARGE screens.
 // If large nav bar is shown
