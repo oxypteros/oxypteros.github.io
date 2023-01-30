86413c5195d3987413d5a492847a4a0223297b82
Oxypteros <oxypteros@gmail.com>
2023-01-30 17:46:20 +0100

    pwa update

--- lab/oxy/pwa-worker.js
+++ lab/oxy/pwa-worker.js
@@ -20,9 +20,11 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  //'./', // Alias for index.html
+  './', // Alias for index.html
   '/js/main.js',
   'css/styles.css',
+  'css/styles-small.css',
+  'css/styles-medium.css',
   'fonts/Inter-roman.var.woff2',
   'fonts/Inter-italic.var.woff2'
 ];
