bec5faec647dce9492ae4c1f9c9de1d6312d741b
Oxypteros <oxypteros@gmail.com>
2023-02-06 13:22:51 +0100

    update logo

--- lab/oxy/js/oxyBody.js
+++ lab/oxy/js/oxyBody.js
@@ -1,23 +1,48 @@
 'use strict';
 /*
-* Oxy Theme
+Oxy Theme
 */
-// Button constant
-const btnScheme = document.querySelector(".btnSchemeJs");
-if(btnScheme){
-  function changeOxyTheme(){
-    let oxyTheme = localStorage.getItem('oxy-theme');
-    if (oxyTheme === 'dark'){
-      document.documentElement.setAttribute('oxy-theme', 'light')
-      localStorage.setItem('oxy-theme', 'light');
-    } else {
-      document.documentElement.setAttribute('oxy-theme', 'dark')
-      localStorage.setItem('oxy-theme', 'dark');
-    }
+
+// Check for CSS support for oxy-theme
+let supportCSS = CSS.supports("color", "var(--bgc)");
+// If oxy-theme supported by CSS enable else hide button
+if ((supportCSS = true)) {
+  // Browser is set to dark true/false
+  const prefersDarkScheme = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  // User preference dark/light/null
+  let oxyTheme = localStorage.getItem("oxy-theme");
+
+  // If visitor had already selected dark set preference to dark change oxy-theme to dark
+  if (oxyTheme === "dark") {
+    document.body.classList.toggle("oxy-dark");
   }
-  // Button Event Listener
-  btnScheme.addEventListener('click', changeOxyTheme);
+  // If first time visitor check browser for color theme and pass it to storage
+  if (oxyTheme === null && prefersDarkScheme === true) {
+    document.body.classList.toggle("oxy-dark");
+    localStorage.setItem("oxy-theme", "dark");
+  }
+
+  // Button constant
+  const btnTheme = document.querySelector(".btnThemeJs");
+  if (btnTheme) {
+    function changeOxyTheme() {
+      let oxyTheme = localStorage.getItem("oxy-theme");
+      if (oxyTheme === "dark") {
+        document.body.classList.toggle("oxy-dark");
+        localStorage.setItem("oxy-theme", "light");
+      } else {
+        document.body.classList.toggle("oxy-dark");
+        localStorage.setItem("oxy-theme", "dark");
+      }
+    }
+    // Button Event Listener
+    btnTheme.addEventListener("click", changeOxyTheme);
+  }
+} else {
+  btnTheme.classList.add("hidden"); // If browser don't support Oxy Theme hide the change theme button.
 }
 /*
-* END Oxy Theme
-*/
+ * END Oxy Theme
+ */
--- lab/oxy/js/oxyHead.js
+++ lab/oxy/js/oxyHead.js
@@ -1,29 +1,4 @@
 'use strict';
 /*
-Oxy Theme
-*/
-// Check for CSS support for oxy-theme
-let supportCSS = CSS.supports('color', 'var(--bgc)');
-// If oxy-theme supported by CSS enable else hide button
-if (supportCSS = true) {
-  // Browser is set to dark true/false
-  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  // User preference dark/light/null
-  let oxyTheme = localStorage.getItem('oxy-theme');
-  // If new visitor check browser for color theme and pass it to storage
-  if (oxyTheme === null && prefersDarkScheme === true ) {
-    document.documentElement.setAttribute('oxy-theme', 'dark')
-    localStorage.setItem('oxy-theme', 'dark');
-  } else if(oxyTheme === null && prefersDarkScheme === false){
-    localStorage.setItem('oxy-theme', 'light');
-  }
-  // If user already set preference to dark change oxy-theme to dark
-  if(oxyTheme === 'dark'){
-    document.documentElement.setAttribute('oxy-theme', 'dark');
-  }
-} else {
-  btnScheme.classList.add('hidden');// If browser don't support Oxy Theme hide the change theme button.
-}
-/*
-* END Oxy Theme
+* Head Js
 */
