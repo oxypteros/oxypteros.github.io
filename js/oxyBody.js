9155d001d7a1d485fef0e6d9bc6d2d054ec630c5
Oxypteros <oxypteros@gmail.com>
2023-02-07 16:33:05 +0100

    create oxyBody.js

---
+++ js/oxyBody.js
@@ -0,0 +1,15 @@
+'use strict';
+co
+/*
+const navSmall = document.querySelector('.nav-small');
+const navMedium = document.querySelector('.nav-medium');
+  function closeSmallMenu() {
+    // Hide nav menu container and show button bar
+    if(navBtnBarSmall.classList.contains('hidden')) {
+      navMenuSmall.classList.add('hidden');
+      navBtnBarSmall.classList.remove('hidden');
+    }
+  };
+  // Event listeners for open close buttons and click outside of bounds
+  hamBtnSmall.addEventListener('click', openSmallMenu);
+*/
