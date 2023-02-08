de553873ebe136882e03508b97214f7d2e0fcc57
oxypteros@gmail.com <oxypteros@gmail.com>
2023-02-08 13:02:41 +0100

    add pi

--- Gatekeeper/js/theNet.js
+++ Gatekeeper/js/theNet.js
@@ -1,7 +1,7 @@
 // theNet.js v.0.0.1 easter egg | MIT License | https://www.phoenicus.com/
 /*/ The script will redirect the browser to "hidden" url when a button is clicked and Ctrl+Shift is pressed./*/
 'use strict';
-const pi = document.querySelector(`.the-net`);
+const pi = document.querySelector(`.pi`);
 pi.addEventListener(`click`, function (e) { 
   const currentUrl = window.location.href;
   let targetUrl = `/secret/winner`
