d55a33802622498ef1229657977a78c6f803f5b6
fedoxy <oxypteros@gmail.com>
2023-05-26 18:26:26 +0200

    update

--- lab/alpha/js/top-nav.js
+++ lab/alpha/js/top-nav.js
@@ -2,9 +2,29 @@ const pageTitle = document.querySelector('.pageTitle');
 const topBarTitle = document.querySelector('.topBarTitle');
 const topBar = document.querySelector('.topBar');
 const height = document.documentElement.clientHeight;
+const element = document.getElementById("main")
+const element1 = document.getElementById("start")
+console.log( height/2)
 if(topBarTitle) {
+  
   document.addEventListener("scroll", (event) => {
+    let scrollFromTop = document.documentElement.scrollTop;
     let yPos = window.scrollY;
+    if(scrollFromTop < height/2 && scrollFromTop != 0 ){
+      element.scrollIntoView();
+      topBarTitle.classList.add('o-1')
+      topBarTitle.textContent = pageTitle.textContent;
+      topBar.classList.add('bb-o');
+      console.log('1st:' + yPos);
+    } else if(scrollFromTop < height && scrollFromTop > 0) {
+      window.scrollTo(0, 0);
+     //element1.scrollIntoView({ block: "start", behavior: "smooth"});
+      topBarTitle.textContent = null;
+      topBar.classList.remove('bb-o');
+      topBarTitle.classList.remove('o-1');
+      console.log('2nd: ' + yPos);
+    }
+    /*
     if(yPos >= height){
       topBarTitle.classList.add('o-1')
       topBarTitle.textContent = pageTitle.textContent;
@@ -12,7 +32,7 @@ if(topBarTitle) {
     }else{
       topBarTitle.textContent = null;
       topBar.classList.remove('bb-o');
-      topBarTitle.classList.remove('o-1')
-    }
+      topBarTitle.classList.remove('o-1') 
+    }*/
   })
 }
